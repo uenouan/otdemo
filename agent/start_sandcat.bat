f84808b640e02db62296adebdbdcc2b23b78f534
uenouan <uenotakabon@gmail.com>
2025-05-20 17:54:47 +0900

    updated

--- agent/start_sandcat.bat
+++ agent/start_sandcat.bat
@@ -1,1 +1,2 @@
-powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "C:\Users\Public\start_sandcat.ps1"
+@echo off
+powershell -Command "Start-Process powershell -Verb RunAs -ArgumentList '-ExecutionPolicy Bypass -WindowStyle Hidden -File \"C:\Users\Public\start_sandcat.ps1\"'"
